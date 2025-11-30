aa960b3808b7351e5a7971b56d7286c73a815a4e
nezihatklc <nezihatkilic38@gmail.com>
2025-11-30 21:01:01 +0300

    Tables getter setter

--- src/main/java/com/project/cms/model/User.java
+++ src/main/java/com/project/cms/model/User.java
@@ -1,1 +1,48 @@
 package com.project.cms.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class User {
+
+    private int userId;
+    private String username;
+    private String passwordHash;
+    private String name;
+    private String surname;
+    private String phone;
+    private LocalDate birthDate;
+    private Role role;
+    private LocalDateTime createdAt;
+
+    public User() {}
+
+    // ---- Getter - Setter ----
+
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getSurname() { return surname; }
+    public void setSurname(String surname) { this.surname = surname; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public LocalDate getBirthDate() { return birthDate; }
+    public void setBirthDate(LocalDate birthDate) { this.birthDate = birthDate; }
+
+    public Role getRole() { return role; }
+    public void setRole(Role role) { this.role = role; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
