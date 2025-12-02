c9b65e7b9b04e067c7eb370deebbfb1402c9049c
Simay Mutlu <simay706@gmail.com>
2025-12-02 16:16:18 +0300

    added StatisticsService and StatisticsServiceImpl

--- src/main/java/com/project/cms/service/StatisticsService.java
+++ src/main/java/com/project/cms/service/StatisticsService.java
@@ -1,6 +1,15 @@
 package com.project.cms.service;
 
-public class StatisticsService {
-    
+import com.project.cms.exception.AppExceptions.ValidationException;
+import com.project.cms.model.User;
+
+
+ /* Provides access to contact statistics. */
+ /* Only the Manager role can use these operations. */
+
+public interface StatisticsService {
+
+    /* Manager-only: get total number of contacts in the system */
+    int getTotalContactCount(User performingUser) throws ValidationException;
+
 }
-  
