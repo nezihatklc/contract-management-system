315d878b70616592d7d0a0b52a512b43b2063383
silasmsk <zsimsek@stu.khas.edu.tr>
2025-12-03 23:33:19 +0300

    App.Java added

--- src/main/java/com/project/cms/app/App.java
+++ src/main/java/com/project/cms/app/App.java
@@ -1,35 +1,7 @@
 package com.project.cms.app;
 
-import com.project.cms.service.ContactService;
-import com.project.cms.service.StatisticsService;
-import com.project.cms.service.UserService;
-import com.project.cms.ui.menu.MainMenu;
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        printAsciiArt();
-        
-        // Initialize Services
-        UserService userService = new UserService();
-        ContactService contactService = new ContactService();
-        StatisticsService statisticsService = new StatisticsService();
-        Scanner scanner = new Scanner(System.in);
-
-        // Start Main Menu
-        MainMenu mainMenu = new MainMenu(userService, contactService, statisticsService, scanner);
-        mainMenu.show();
-        
-        scanner.close();
-        System.out.println("Goodbye!");
-    }
-
-    private static void printAsciiArt() {
-        System.out.println("   ______            __                  __ ");
-        System.out.println("  / ____/___  ____  / /Ry__  ____ ______/ /_");
-        System.out.println(" / /   / __ \\/ __ \\/ __/ __ `/ ___/ ___/ __/");
-        System.out.println("/ /___/ /_/ / / / / /_/ /_/ / /__/ /__/ /_  ");
-        System.out.println("\\____/\\____/_/ /_/\\__/\\__,_/\\___/\\___/\\__/  ");
-        System.out.println("                                            ");
+        ApplicationInitializer.start();
     }
 }
--- src/main/java/com/project/cms/app/ApplicationInitializer.java
+++ src/main/java/com/project/cms/app/ApplicationInitializer.java
@@ -1,5 +1,50 @@
 package com.project.cms.app;
 
+import com.project.cms.dao.contact.ContactDao;
+import com.project.cms.dao.contact.ContactDaoImpl;
+
+import com.project.cms.service.UserService;
+import com.project.cms.service.UserServiceImpl;
+
+import com.project.cms.service.ContactService;
+import com.project.cms.service.ContactServiceImpl;
+
+import com.project.cms.service.StatisticsService;
+
+import com.project.cms.ui.animation.AsciiAnimator;
+import com.project.cms.ui.menu.MainMenu;
+import com.project.cms.ui.input.ConsolePrinter;
+
 public class ApplicationInitializer {
-    
+
+    public static void start() {
+        try {
+            // DAO needed for the StatisticsService
+            ContactDao contactDao = new ContactDaoImpl();
+
+            // Services
+            UserService userService = new UserServiceImpl();
+            ContactService contactService = new ContactServiceImpl();
+            StatisticsService statisticsService = new StatisticsService(contactDao);
+
+            // Welcome animation
+            AsciiAnimator.showWelcome();
+
+            // Main menu
+            MainMenu mainMenu = new MainMenu(
+                    userService,
+                    contactService,
+                    statisticsService
+            );
+
+            mainMenu.start();
+
+        } catch (Exception e) {
+            ConsolePrinter.error("A critical error occurred: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            // Goodbye animation always runs
+            AsciiAnimator.showGoodbye();
+        }
+    }
 }
