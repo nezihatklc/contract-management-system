bc79af7fd70124a72050c990668378bb55c40488
silasmsk <zsimsek@stu.khas.edu.tr>
2025-12-03 22:27:41 +0300

    SearchCriteria added

--- src/main/java/com/project/cms/model/SearchCriteria.java
+++ src/main/java/com/project/cms/model/SearchCriteria.java
@@ -4,19 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SearchCriteria {
-    
-    private Map<String, String> criteria = new HashMap<>();
 
-    //  "add" :
-    public void add(String field, String value) {
-        if (value != null && !value.trim().isEmpty()) {
+    private final Map<String, Object> criteria = new HashMap<>();
+
+    
+    public void add(String field, Object value) {
+        if (value == null) return;
+
+        String text = value.toString().trim();
+        if (!text.isEmpty()) {
             criteria.put(field, value);
         }
     }
 
-    public Map<String, String> getCriteria() {
+    
+    public Map<String, Object> getCriteria() {
         return criteria;
     }
+
     
     public boolean hasCriteria() {
         return !criteria.isEmpty();
